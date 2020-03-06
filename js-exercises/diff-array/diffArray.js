95496a365da0f77d00df353582703747c71f0ed4
Arfat Salman <giney.paradise@gmail.com>
2020-03-06 10:12:54 +0530

    Remove implementation of diff-array

--- js-exercises/diff-array/diffArray.js
+++ js-exercises/diff-array/diffArray.js
@@ -1,19 +1,5 @@
-function diffArray(firstArray, secondArray) {
-  const firstUnique = firstArray.reduce((acc, el) => {
-    if (secondArray.indexOf(el) === -1) {
-      return [...acc, el];
-    }
-    return acc;
-  }, []);
-
-  const secondUnique = secondArray.reduce((acc, el) => {z
-    if (firstArray.indexOf(el) === -1) {
-      return [...acc, el];
-    }
-    return acc;
-  }, []);
-
-  return firstUnique.concat(secondUnique);
+function diffArray() {
+  
 }
 
 export {
